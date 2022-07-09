7899a7ae06bd4ca97d51376b86818a0702a2025e
m.saharov <maxim.saharov501@gmail.com>
2022-07-09 14:27:22 +0300

    2.19 – WebSocket – part 2

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,10 +1,9 @@
 //
 import React, {useEffect, useState} from 'react'
+import {Spin} from 'antd'
 import {TextAreaOrInputOnChangeType} from '../../types/types'
+import {outputDateSeconds, uniqueIdGetTimeInStringPlusIndex} from '../../utils/object-helpers'
 
-
-const wsChannel = new WebSocket(
-   'wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
 
 export type ChatMessagesType = {
    message: string
@@ -14,7 +13,6 @@ export type ChatMessagesType = {
 }
 
 const ChatPage: React.FC = () => {
-
    return (
       <div>
          <Chat />
@@ -25,36 +23,93 @@ const ChatPage: React.FC = () => {
 
 const Chat: React.FC = () => {
 
+   const [wsChannel, setWsChannel] = useState<WebSocket | null>(null)
+   const [isWsChannelOpen, setIsWsChannelOpen] = useState(false)
+
+
+   useEffect(() => {
+
+      let ws: WebSocket
+
+      const closeHandler = () => {
+         console.log('close с addEventListener', outputDateSeconds())
+         setIsWsChannelOpen(false)
+         setTimeout(createChanel, 5000)
+      }
+
+      function createChanel() {
+
+         if (ws !== undefined) {
+            ws.removeEventListener('close', closeHandler)
+            ws.close()
+            console.log('просто удалили подписку', outputDateSeconds())
+         }
+
+
+         ws = new WebSocket(
+            'wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
+
+         ws.addEventListener('close', closeHandler)
+
+         setWsChannel(ws)
+
+      }
+
+      createChanel()
+
+      return () => {
+         ws.removeEventListener('close', closeHandler)
+         ws.close()
+      }
+
+   }, [])
+
+
    return (
       <div>
-         <Messages />
-         <AddMessageForm />
+         <Messages wsChannel={wsChannel} />
+         <AddMessageForm
+            wsChannel={wsChannel}
+            setWsChannelOpen={setIsWsChannelOpen}
+            isWsChannelOpen={isWsChannelOpen} />
       </div>
    )
 }
 
-const Messages: React.FC = () => {
+const Messages: React.FC<{ wsChannel: WebSocket | null }> = ({wsChannel}) => {
 
    const [messages, setMessages] = useState<ChatMessagesType[]>([])
 
+   console.log(messages)
+
    useEffect(() => {
 
-         wsChannel.addEventListener('message', (e: MessageEvent) => {
-               const dataArray = JSON.parse(e.data)
+         const messageHandler = (e: MessageEvent) => {
+            const newMassages = JSON.parse(e.data)
 
-               setMessages((prevMessages) => [...prevMessages, ...dataArray])
-            }
-         )
+            setMessages((prevMessages) => [...prevMessages, ...newMassages])
+         }
 
-      }, []
+         wsChannel?.addEventListener('message', messageHandler)
+
+         return () => {
+            wsChannel?.removeEventListener('message', messageHandler)
+         }
+
+      }, [wsChannel]
    )
 
    return (
       <div style={{height: '400px', overflowY: 'auto'}}>
-         {messages.map((m, index) => <MessageChat key={index} message={m} />)}
+         {messages.map((m, index) => {
+            const uniqueId = uniqueIdGetTimeInStringPlusIndex(index)
+            return <MessageChat key={uniqueId} message={m} />
+         })}
       </div>
    )
 }
+
 
 const MessageChat: React.FC<{ message: ChatMessagesType }> = ({message}) => {
 
@@ -71,39 +126,105 @@ const MessageChat: React.FC<{ message: ChatMessagesType }> = ({message}) => {
 }
 
 
-const AddMessageForm: React.FC = () => {
+const AddMessageForm: React.FC<{
+   wsChannel: WebSocket | null
+   setWsChannelOpen: any
+   isWsChannelOpen: boolean
+}> = ({wsChannel, setWsChannelOpen, isWsChannelOpen}) => {
 
    const [message, setMessage] = useState('')
+   const [readyStatus, setReadyStatus] = useState<'pending' | 'ready'>('pending')
 
+
+   console.log(readyStatus, outputDateSeconds())
+
+
+   useEffect(() => {
+
+      const openHandler = () => {
+         console.log('open с addEventListener', outputDateSeconds())
+
+         console.log(readyStatus)
+
+         setReadyStatus('ready')
+
+         setWsChannelOpen(true)
+      }
+
+      wsChannel?.addEventListener('open', openHandler)
+
+      return () => {
+         wsChannel?.removeEventListener('open', openHandler)
+      }
+
+// eslint-disable-next-line
+   }, [wsChannel])
+
+
+   //region Description
    const onChangeTextArea = (event: TextAreaOrInputOnChangeType) => {
-
       setMessage(event.target.value)
    }
 
-   const sendMassages = () => {
+   const onSendMassagesButton = () => {
+      onSendMessage()
+   }
 
+   const onKeyPressInTextArea = (event: any) => {
+      if (event.ctrlKey && event.code === 'Enter') {
+         onSendMessage()
+      }
+   }
+
+   const onSendMessage = () => {
       if (!message) {
+         alert('Пустое сообщение невозможно отправить!')
          return
       }
 
-      wsChannel.send(message)
+      if (message.length > 100) {
+         alert(
+            `Можно отправлять не более 100 знаков,
+            а сейчас уже ${message.length}`)
+         return
+      }
+
+      wsChannel?.send(message)
 
       setMessage('')
+
    }
+
+   const isDisabledButton = wsChannel === null
+      || readyStatus !== 'ready'
+      || !isWsChannelOpen
+
+
+   const placeholderText =
+      `Press Ctrl + Enter to send a message or button "Send".\nYou can't send more than 100 symbols.`
+   //endregion
+
 
    return (
       <div>
          <div>
             <textarea
+               style={{height: '100px', width: '250px'}}
                onChange={onChangeTextArea}
                value={message}
+               placeholder={placeholderText}
+               onKeyPress={onKeyPressInTextArea}
             />
          </div>
 
          <div>
-            <button onClick={sendMassages}>Send</button>
+            {isDisabledButton
+               ? <Spin />
+               : <button
+                  onClick={onSendMassagesButton}>Send
+               </button>
+            }
          </div>
-
       </div>
    )
 }
@@ -112,14 +233,5 @@ const AddMessageForm: React.FC = () => {
 export default ChatPage
 
 
-// {
-//    message: "asdasd",
-//       photo:        "https://social-network.samuraijs.com/activecontent/images/users/2/user-small.jpg?v=4",
-//    userId: 2,
-//    userName: "samurai "
-// }
-
-// message: "Привет, есть тут кто ?)2"
-// photo: "https://social-network.samuraijs.com/activecontent/images/users/23275/user-small.jpg?v=12"
-// userId: 23275
-// userName: "Maxim Saharov"
+//текст длинной более 100 знаков и без косых здесь (тут спам - ааапппппппппннннннннпппппп) и тут конец
+//console.log('ушли с компоненты AddMessageForm', outputDateSeconds())
--- src/utils/object-helpers.ts
+++ src/utils/object-helpers.ts
@@ -11,3 +11,24 @@ export const updateObjectInArray = (array: any, someValue: any,
    )
 }
 
+
+export const outputDateSeconds = () => {
+   const date = new Date()
+   let output = String(
+      date.getHours()
+      + ':' + date.getMinutes()
+      + ':' + date.getSeconds()
+      + ':' + date.getMilliseconds()
+   )
+   output = output + ''
+   return output
+}
+
+
+export const uniqueIdGetTimeInStringPlusIndex = (index: number | string) => {
+   const date = new Date()
+   let output = String(date.getTime())
+   output = output + '_' + index
+   return output
+}
+
